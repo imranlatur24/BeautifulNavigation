95270593ec729a44b31679afb8d77130e64891a4
imran <imranlatur24@gmail.com>
2022-02-11 16:59:28 +0530

    dashbaord commit latest

--- app/src/main/java/com/novetrics/beautifulnavigation/modal/Horizontal_Menus2.java
+++ app/src/main/java/com/novetrics/beautifulnavigation/modal/Horizontal_Menus2.java
@@ -3,38 +3,20 @@ import com.novetrics.beautifulnavigation.R;
 
 public class Horizontal_Menus2 {
     public  static String[] menus=new String[]{
-            "Menu 1",
-            "Menu 2",
-            "Menu 3",
-            "Menu 4",
-            "Menu 5",
-            "Menu 6",
-            "Menu 7",
-            "Menu 8",
-            "Menu 9",
-            "Menu 10",
-            "Menu 11",
-            "Menu 12",
-            "Menu 13",
-            "Menu 14",
-            "Menu 15"
+            "Dictionary",
+            "Members",
+            "My \nProfile",
+            "Change \nPassword",
+            "About \nus",
+            "Contact \nus",
     };
 
     public static  int[] logopath = new int[]{
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery,
-            R.drawable.ic_gallery
+            R.drawable.ic_dictionary,
+            R.drawable.ic_members,
+            R.drawable.ic_profile,
+            R.drawable.ic_change_password,
+            R.drawable.ic_aboutus,
+            R.drawable.ic_baseline_contact_us_24,
     };
 }
